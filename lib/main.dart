b1c36b542eec08233261684703df537930f3dd34
Kamil Powałowski <hello@kamil.id>
2023-05-28 12:16:38 +0200

    feat: Changed URL strategy to remove # from urls.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:url_strategy/url_strategy.dart';
+
 import 'package:habitquokka/app.dart';
 
-void main() => runApp(const App());
+void main() {
+  setPathUrlStrategy();
+  runApp(const App());
+}
